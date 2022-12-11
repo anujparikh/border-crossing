0380ad5cd87fe4f54f724925905c8efeb20b03ec
Anuj Parikh <aparikh@roku.com>
2022-12-10 20:12:47 -0600

    saving fetched data to mysql DB

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,27 @@
 package org.example;
 
+import static org.example.persistence.DbManager.DB_URL;
+import static org.example.persistence.DbManager.TABLE_NAME;
+
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
+import org.example.persistence.DbManager;
+
+import java.util.Properties;
 
 public class Main {
 
   private static final String APP_NAME = "BroderCrossing";
   private static final String LOCAL_NODE_ID = "local";
   private static final String FORMAT = "csv";
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     Main main = new Main();
     main.init();
   }
 
-  private static void init() {
+  private static void init() throws Exception {
     // Create the Spark session on the localhost master node
     SparkSession sparkSession = SparkSession.builder()
         .appName(APP_NAME)
@@ -25,6 +32,16 @@ public class Main {
     Dataset<Row> df = sparkSession.read().format(FORMAT)
         .option("header", "true")
         .load("src/main/resources/spark-data/Border_Crossing_Entry_Data.csv");
+
+    // Start DB connection
+    DbManager dbManager = new DbManager();
+    dbManager.startDB();
+
+    Properties prop = dbManager.buildDBProperties();
+    df.write().mode(SaveMode.Overwrite)
+            .jdbc(DB_URL, TABLE_NAME, prop);
+
+    dbManager.displayTableResults(5);
 
     df.show(15);
   }
